242e3617e77c239eab463396f77be803f98fd9ed
Mikey <michael.luevanos@gmail.com>
2021-08-11 13:11:43 -0700

    added question mark to key

--- decryption.py
+++ decryption.py
@@ -19,7 +19,7 @@ i = 2
 d =int(((i * phi) + 1)/e)
 
 #repeating the encryption_dict, I could not figure out how to import it from encryption.py
-encryption_dict = {'A':1, 'B':2, 'C':3, 'D':4, 'E': 5, 'F':6, 'G':7, 'H':8, 'I':9, 'J':10, 'K':11, 'L':12, 'M':13, 'N':14, 'O':15, 'P':16, 'Q':17, 'R':18, 'S':19, 'T':20, 'U':21, 'V':22, 'W':23, 'X':24, 'Y':25, 'Z': 26, ' ':27, '.':28, "\'":29, ',':30, '!': 31} 
+encryption_dict = {'A':1, 'B':2, 'C':3, 'D':4, 'E': 5, 'F':6, 'G':7, 'H':8, 'I':9, 'J':10, 'K':11, 'L':12, 'M':13, 'N':14, 'O':15, 'P':16, 'Q':17, 'R':18, 'S':19, 'T':20, 'U':21, 'V':22, 'W':23, 'X':24, 'Y':25, 'Z': 26, ' ':27, '.':28, "\'":29, ',':30, '!': 31, '?' : 32} 
 
 #Decrypt using a private key
 # decrypted = c^d mod n
--- encryption.py
+++ encryption.py
@@ -22,7 +22,7 @@ e = 5
     #2: Apply the encoding formula to the resulting sequence of numbers
     #*Stick to only lowercase of only uppercase letters to reduce complexity of the program
     
-encryption_dict = {'A':1, 'B':2, 'C':3, 'D':4, 'E': 5, 'F':6, 'G':7, 'H':8, 'I':9, 'J':10, 'K':11, 'L':12, 'M':13, 'N':14, 'O':15, 'P':16, 'Q':17, 'R':18, 'S':19, 'T':20, 'U':21, 'V':22, 'W':23, 'X':24, 'Y':25, 'Z': 26, ' ':27, '.':28, "\'":29, ',':30, '!': 31} 
+encryption_dict = {'A':1, 'B':2, 'C':3, 'D':4, 'E': 5, 'F':6, 'G':7, 'H':8, 'I':9, 'J':10, 'K':11, 'L':12, 'M':13, 'N':14, 'O':15, 'P':16, 'Q':17, 'R':18, 'S':19, 'T':20, 'U':21, 'V':22, 'W':23, 'X':24, 'Y':25, 'Z': 26, ' ':27, '.':28, "\'":29, ',':30, '!': 31, '?' : 32} 
 
 #Function that takes in a string, and converts it to a list of numbers
 def translate_string(string):
